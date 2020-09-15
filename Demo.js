6bb721e74c2384dad8eb31a784bb43b2b4f8b0ab
Ayush Basnet <68268620+ayushbasnet@users.noreply.github.com>
2020-09-15 13:47:06 +0545

    Added CSS And Some Parts Of Javascripts

--- Demo.js
+++ Demo.js
@@ -1,1 +1,16 @@
-console.log('Hi There!');
+//Selectors
+
+const todoInput = document.queryselector('.toDoInput');
+const todoButton = document.queryselector('.toDoButton');
+const todoList = document.queryselector('.toDoList');
+
+//Events Listeners
+todoButton.addEventListener('click', addTodo);
+
+//functions
+function addTodo(event) {
+  //Prevent Form From Submitting
+
+  event.preventDefault();
+  console.log();
+}
