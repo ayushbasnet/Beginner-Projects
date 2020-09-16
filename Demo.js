0c90c5f044ddccaadc7bd841d3d75401f2043bfc
ayushbasnet <ayushbasnet@github.com>
2020-09-16 13:05:58 +0545

    Added Some Fonts From Font Awesome and also a JS Function For Adding and Removing Items in the List.

--- Demo.js
+++ Demo.js
@@ -1,8 +1,8 @@
 //Selectors
 
-const todoInput = document.queryselector('.toDoInput');
-const todoButton = document.queryselector('.toDoButton');
-const todoList = document.queryselector('.toDoList');
+const todoInput = document.querySelector('.toDoInput');
+const todoButton = document.querySelector('.toDoButton');
+const todoList = document.querySelector('.toDoList');
 
 //Events Listeners
 todoButton.addEventListener('click', addTodo);
@@ -10,7 +10,30 @@ todoButton.addEventListener('click', addTodo);
 //functions
 function addTodo(event) {
   //Prevent Form From Submitting
-
   event.preventDefault();
-  console.log();
+
+  //Todo div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  //Create LI
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+
+  //Check mark
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+  completedButton.classList.add('completed-btn');
+  todoDiv.appendChild(completedButton);
+
+  //Trash mark
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+  trashButton.classList.add('completed-btn');
+  todoDiv.appendChild(trashButton);
+
+  //Append to List
+  todoList.appendChild(todoDiv);
 }
